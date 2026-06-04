cd0114a10a2803fe346312d3e855286a52ed2b34
VIA IS GOURGEOUS <butterscotchvia@gmail.com>
2026-06-04 19:52:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function setMood(mood){
 
+  document.getElementById("result").innerText =
+  "Today's mood: " + mood;
+
+  localStorage.setItem("mood", mood);
+}
+
+window.onload = function(){
+
+  const mood = localStorage.getItem("mood");
+
+  if(mood){
+
+    document.getElementById("result").innerText =
+    "Today's mood: " + mood;
+
+  }
+
+}
